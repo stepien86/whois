72bc73dad95e1a5419dc912d4e6a72df17d2cbaa
Łukasz Stępień <info@itol.pl>
2022-11-05 22:46:02 +0100

    whois bash script

---
+++ whois.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input="ipList.txt"
+  while IFS= read -r line 
+    do
+      date=$(echo $line |cut -d ';' -f 1)
+      IP=$(echo $line |cut -d ';' -f 2)
+      netName=$(whois $IP | grep 'netname\|OrgName' -m 1 | cut --complement -d " " -f 1 | tr -d "[:space:]")
+      descr=$(whois $IP | grep descr -m 1 | cut --complement -d " " -f 1 | tr -d "[:space:]")
+      country=$(whois $IP | grep 'country\|Country' -m 1 | cut --complement -d " " -f 1 | tr -d "[:space:]" )
+        echo "$date;$IP;$netName;$descr;$country">>result.txt
+    done <$input
